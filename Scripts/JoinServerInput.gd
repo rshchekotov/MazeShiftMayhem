375e4b4b54ad2474aaf3dfab241c8e3e064991a1
asocial <roman.chernikov@madcap.one>
2023-12-17 01:25:09 +0100

    add debug messages on trying to connect to the server
    - change jump button to be only "SPACE" by creating new ui_jump key bind

--- Scripts/JoinServerInput.gd
+++ Scripts/JoinServerInput.gd
@@ -2,4 +2,5 @@ extends LineEdit
 @onready var serverScript = $".."
 
 func _on_text_submitted(new_text):
-	serverScript.join_game(new_text)
+	print(new_text)
+	print(serverScript.join_game(new_text))
