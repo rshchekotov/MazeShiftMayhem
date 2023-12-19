81ff4795ac0eb218447692b61331f7c79d7b298d
asocial <roman.chernikov@madcap.one>
2023-12-19 13:34:58 +0100

    fix the co-op player position being different from it's true position
    - simplify the coop to 2 scripts: one for the player and one for the connection manager called "multiplayer_core"
    - simplify the level map so that it doesn't need a dedicated script, "multiplayer_core" now handels the player spawning and despawning
    - simplify the player structure: it does not need a dedicated input synchroniser, insted all it is all done in the "multi_player" script
    - solve problem with only the host being able to move while synchronising the positio and rotation

--- Scripts/multi_player.gd
+++ Scripts/multi_player.gd
@@ -2,58 +2,52 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENSITIVITY = 0.005
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@onready var camera = $Camera3D
 
-# Set by the authority, synchronized on spawn.
-@export var player := 1 :
-	set(id):
-		player = id
-		# Give authority over the player input to the appropriate peer.
-		$PlayerInput.set_multiplayer_authority(id)
-
-# Player synchronized input.
-@onready var input = $PlayerInput
-@onready var neck = $Neck
-@onready var camera = $Neck/Camera3D
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
 
 func _ready():
-	# Set the camera as current if we are this player.
-	if player == multiplayer.get_unique_id():
-		$Neck/Camera3D.current = true
-		neck = $Neck
-		camera = $Neck/Camera3D
-	# Only process on server.
-	# EDIT: Left the client simulate player movement too to compesate network latency.
-	# set_physics_process(multiplayer.is_server())
+	if not is_multiplayer_authority(): return
+	camera.current = true
+
+func _unhandled_input(event):
+	if not is_multiplayer_authority(): return
+	
+	if event is InputEventMouseButton:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	elif event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+			camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+			camera.rotation.x = clamp(camera.rotation.x,   deg_to_rad(-60),  deg_to_rad(60))
 
 func _physics_process(delta):
+	if not is_multiplayer_authority(): return
 	# Add the gravity.
+	var input_direction = Input.get_vector("player_left", "player_right", "player_forward", "player_backward")
+	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if input.jumping and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Reset jump state.
-	input.jumping = false
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY 
 
 	# Handle movement.
-	var new_direction = (neck.transform.basis * Vector3(input.direction.x, 0, input.direction.y)).normalized()
+	var new_direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
 	if new_direction:
 		velocity.x = new_direction.x * SPEED
 		velocity.z = new_direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
 	move_and_slide()
-	
-func _input(event):
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			neck.rotation.y += input.view_point.x
-			camera.rotation.x += input.view_point.y
--- Scripts/multiplayer_core.gd
+++ Scripts/multiplayer_core.gd
@@ -1,66 +1,69 @@
 extends Node
 
 const PORT = 7000
+const DEFAULT_LEVEL = "res://Scenes/map.tscn"
+const DEFAULT_IP_ADRESS = "127.0.0.1"
+var peer 
+
+const SPAWN_DISPERSION_RADIUS = 3
+const Player = preload("res://Scenes/multi_player.tscn")
+
+@onready var UI = $UI
+@onready var Level_Files = $Level
+@onready var Player_Files = $Players
 
 func _ready():
-	# Start paused
 	get_tree().paused = true
-	# You can save bandwith by disabling server relay and peer notifications.
-	multiplayer.server_relay = false
-
-	# Automatically start the server in headless mode.
-	if DisplayServer.get_name() == "headless":
-		print("Automatically starting dedicated server")
-		_on_host_pressed.call_deferred()
+	peer = ENetMultiplayerPeer.new()
 
 
 func _on_host_pressed():
 	# Start as server
-	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT)
 	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		OS.alert("Failed to start multiplayer server")
+		print("Failed to start multiplayer server")
 		return
 	multiplayer.multiplayer_peer = peer
 	start_game()
 
 
-func _on_connect_by_ip_text_submitted(txt):
-	# Start as client
+func _on_server_ip_text_submitted(txt):
+	print("hi")
 	if txt == "":
-		OS.alert("Need a remote to connect to.")
-		return
-	var peer = ENetMultiplayerPeer.new()
+		txt = DEFAULT_IP_ADRESS
 	peer.create_client(txt, PORT)
 	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		OS.alert("Failed to start multiplayer client")
+		print("Failed to start multiplayer client")
 		return
 	multiplayer.multiplayer_peer = peer
 	start_game()
 
 func start_game():
-	# Hide the UI and unpause to start the game.
-	$UI.hide()
+	UI.hide()
 	get_tree().paused = false
-	# Only change level on the server.
-	# Clients will instantiate the level via the spawner.
 	if multiplayer.is_server():
-		change_level.call_deferred(load("res://Scenes/LevelBase.tscn"))
+		multiplayer.multiplayer_peer = peer
+		multiplayer.peer_connected.connect(add_player)
+		multiplayer.peer_disconnected.connect(del_player)
+		change_level.call_deferred(load(DEFAULT_LEVEL))
+		add_player.call_deferred(multiplayer.get_unique_id())
 
-
-# Call this function deferred and only on the main authority (server).
+##call only deffered, so that the level has time to load 
 func change_level(scene: PackedScene):
-	# Remove old level if any.
-	var level = $Level
+	var level = Level_Files
 	for c in level.get_children():
 		level.remove_child(c)
 		c.queue_free()
-	# Add new level.
 	level.add_child(scene.instantiate())
 
-# The server can restart the level by pressing HOME.
-func _input(event):
-	if not multiplayer.is_server():
+func add_player(id: int):
+	var character = Player.instantiate()
+	character.position = Vector3(SPAWN_DISPERSION_RADIUS * randf(), 0, SPAWN_DISPERSION_RADIUS * randf())
+	character.name = str(id)
+	character.rotation.x = 0;
+	Player_Files.add_child(character, true)
+
+func del_player(id: int):
+	if not Player_Files.has_node(str(id)):
 		return
-	if event.is_action("ui_home") and Input.is_action_just_pressed("ui_home"):
-		change_level.call_deferred(load("res://Scenes/LevelBase.tscn"))
+	Player_Files.get_node(str(id)).queue_free()
